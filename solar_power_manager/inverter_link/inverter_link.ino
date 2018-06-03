0be3568feb5dac81001beb2bc75da7826748aec4
Lukasz Towarek <lukasz.towarek@gmail.com>
2018-06-03 16:20:08 +0200

    Read QPIGS command from Serial and resend it in hex

---
+++ solar_power_manager/inverter_link/inverter_link.ino
@@ -0,0 +1,36 @@
+String command_string = "";
+bool command_read = false;
+
+void setup() {
+  Serial.begin(2400);
+}
+
+void loop() {
+  if (Serial.available()) {
+    char in = (char)Serial.read();
+    if (in == '\r') {
+      command_read = true;
+      Serial.print("Command read: ");
+      Serial.println(command_string);
+    } else {
+      command_string += in;
+    }
+  }
+
+  if (command_read && command_string == "QPIGS") {
+    Serial.write(0x51);
+    Serial.write(0x50);
+    Serial.write(0x49);
+    Serial.write(0x47);
+    Serial.write(0x53);
+    Serial.write(0xb7);
+    Serial.write(0xa9);
+    Serial.write(0x0d);
+  }
+
+  if (command_read) {
+    command_read = false;
+    command_string = "";
+  }
+}
+
