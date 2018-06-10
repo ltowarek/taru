9717fbc920b5590c8a37623b1e460fa6137fdbc0
Lukasz Towarek <lukasz.towarek@gmail.com>
2018-06-10 19:46:08 +0200

    POC of communication between InfluxDB and Paho

--- solar_power_manager/database_link/database_link.py
+++ solar_power_manager/database_link/database_link.py
@@ -1,13 +1,59 @@
 #!/usr/bin/env python3
 
 import influxdb
+import paho.mqtt.client as mqtt
+import logging
+import os
+
+def on_message(client, user_data, message):
+    logging.debug('Payload: {}'.format(message.payload))
+    logging.debug('Topic: {}'.format(message.topic))
+    user_data.save(os.path.basename(message.topic), int(message.payload))
+
+class DatabaseLink():
+    def __init__(self):
+        self.db_client = None
+        self.mqtt_client = None
+        
+    def initialize(self, host='localhost'):
+        self.db_client = influxdb.InfluxDBClient(host)
+
+        databases = self.db_client.get_list_database()
+        database_name = 'inverter'
+        if database_name not in [d['name'] for d in databases]:
+            self.db_client.create_database(database_name)
+        self.db_client.switch_database(database_name)
+
+        self.mqtt_client = mqtt.Client(userdata=self)
+        self.mqtt_client.on_message = on_message
+        self.mqtt_client.connect(host)
+        self.mqtt_client.subscribe('inverter/test_measurement')
+        
+    def close(self):
+        self.db_client.close()
+        self.mqtt_client.disconnect()
+        
+    def loop(self):
+        self.mqtt_client.loop()
+
+    def loop_start(self):
+        self.mqtt_client.loop_start()
+
+    def loop_stop(self):
+        self.mqtt_client.loop_stop()
+
+    def save(self, measurement, value):
+        self.db_client.write_points([{'measurement': measurement, 'fields': {'value': value}}])
+
 
 if __name__ == '__main__':
-    client = influxdb.InfluxDBClient()
-    databases = client.get_list_database()
-    database_name = 'inverter'
-    if database_name not in databases:
-        client.create_database(database_name)
-    client.switch_database(database_name)
-    client.close()
+    logging.basicConfig(level=logging.DEBUG)
+    link = DatabaseLink()
+    link.initialize()
+    try:
+        while True:
+            link.loop()
+    except KeyboardInterrupt:
+        link.loop_stop()
+        link.close()
 
--- solar_power_manager/test/database_link_test.py
+++ solar_power_manager/test/database_link_test.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python3
 
 import unittest
+import database_link.database_link as db_link
+import paho.mqtt.client as mqtt
 import influxdb
+import time
 
 class DatabaseLinkTest(unittest.TestCase):
-    def test_database_link_does_inverter_database_exist(self):
-        client = influxdb.InfluxDBClient()
-        self.assertIn({'name': 'inverter'}, client.get_list_database())
-        client.close()
+    def test_database_link_writes_points_from_mqtt_message(self):
+        measurement = 'test_measurement'
+        value = 123
+        topic = 'inverter/{}'.format(measurement)
+        db_name = '"inverter"."autogen"."{}"'.format(measurement)
+
+        db_client = influxdb.InfluxDBClient()
+        db_client.switch_database('inverter')
+        db_client.drop_measurement(measurement)
+
+        link = db_link.DatabaseLink()
+        link.initialize()
+        link.loop_start()
+
+        mqtt_client = mqtt.Client()
+        mqtt_client.connect('localhost')
+        mqtt_client.publish(topic, value)
+        mqtt_client.disconnect()
+
+        time.sleep(1)
+        
+        results = db_client.query('SELECT value FROM {}'.format(db_name))
+        points = results.get_points()
+        self.assertIn(value, [p['value'] for p in points])
+
+        link.loop_stop()
+        link.close()
+        db_client.close()
 
 
 if __name__ == '__main__':
