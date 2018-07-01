bf56abfc33947d68594b5ca32494425774011bab
Lukasz Towarek <lukasz.towarek@gmail.com>
2018-07-01 14:49:30 +0200

    Disable database_link logging

--- solar_power_manager/start.sh
+++ solar_power_manager/start.sh
@@ -5,5 +5,5 @@ mkdir -p logs
 broker/start.sh > logs/broker.log 2>&1;
 scheduler/start.sh > logs/scheduler.log 2>&1;
 visualizations/start.sh > logs/visualizations.log 2>&1;
-nohup database_link/start.sh > logs/database_link.log 2>&1 &
+nohup database_link/start.sh > /dev/null 2>&1 &
 
