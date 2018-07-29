e98a220c0b858559ec50fc7da40251cd464c37ea
Lukasz Towarek <lukasz.towarek@gmail.com>
2018-07-29 23:51:34 +0200

    Move scripts to scripts directory

--- solar_power_manager/scripts/install_ubuntu.sh
+++ solar_power_manager/scripts/install_ubuntu.sh
@@ -10,7 +10,7 @@ sudo systemctl disable mosquitto
 sudo apt-get install -y python3 python3-pip python3-virtualenv
 python3 -m virtualenv -p python3 venv
 
-. venv/bin/activate
+. ../venv/bin/activate
 pip install -r requirements.txt
 deactivate
 
