34a705ed1d18e32376957d8479b5f3f9f8ab24f7
Lukasz Towarek <lukasz.towarek@gmail.com>
2018-10-14 16:13:31 +0200

    Add suport for MQTT client

--- temperature_display/display.py
+++ temperature_display/display.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from RPLCD.i2c import CharLCD
+from paho.mqtt.client import Client
 
 
 class LCD:
@@ -33,9 +34,30 @@ class LCD:
         self.close()
 
 
+MQTT_OUTSIDE_TEMPERATURE_TOPIC = 'sensor/balcony/temperature'
+MQTT_INSIDE_TEMPERATURE_TOPIC = 'sensor/office/temperature'
+MQTT_USERNAME = ''
+MQTT_PASSWORD = ''
+MQTT_CLIENT = 'display'
+MQTT_SERVER = ''
+
+
+def on_message(_, lcd, message):
+    topic = message.topic
+    value = float(message.payload)
+    if topic == MQTT_OUTSIDE_TEMPERATURE_TOPIC:
+        lcd.print_outside_temperature(value)
+    elif topic == MQTT_INSIDE_TEMPERATURE_TOPIC:
+        lcd.print_inside_temperature(value)
+
+
 if __name__ == '__main__':
-    temperature_outside = 10.5
-    temperature_inside = 20.4
+    mqtt = Client(client_id=MQTT_CLIENT)
+    mqtt.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+    mqtt.connect(MQTT_SERVER)
+    mqtt.on_message = on_message
+    mqtt.subscribe(MQTT_OUTSIDE_TEMPERATURE_TOPIC)
+    mqtt.subscribe(MQTT_INSIDE_TEMPERATURE_TOPIC)
     with LCD() as lcd:
-        lcd.print_outside_temperature(temperature_outside)
-        lcd.print_inside_temperature(temperature_inside)
+        mqtt.user_data_set(lcd)
+        mqtt.loop_forever()
