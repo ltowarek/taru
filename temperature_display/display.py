5c426995c05349873b441d09f589aa35d8163e61
Lukasz Towarek <lukasz.towarek@gmail.com>
2018-10-14 15:15:18 +0200

    Print degree symbol

--- temperature_display/display.py
+++ temperature_display/display.py
@@ -3,7 +3,9 @@ from RPLCD.i2c import CharLCD
 if __name__ == '__main__':
     temperature_outside = 10
     temperature_inside = 20
+    degree_symbol = chr(223)
     lcd = CharLCD('PCF8574', 0x27)
-    lcd.write_string(f'Out: {temperature_outside}\u2103\n'
-                     f'In: {temperature_inside}\u2103')
-    lcd.close(clear=True)
+    lcd.write_string('Out: {0}{2}C\n\rIn: {1}{2}C'.format(temperature_outside,
+                                                          temperature_inside,
+                                                          degree_symbol))
+    lcd.close()
