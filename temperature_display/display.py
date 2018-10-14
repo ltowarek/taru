f21b739bdebd43e5e866ab99de5a0261790d2392
Lukasz Towarek <lukasz.towarek@gmail.com>
2018-10-14 15:17:01 +0200

    Set temperature as floats

--- temperature_display/display.py
+++ temperature_display/display.py
@@ -3,8 +3,8 @@
 from RPLCD.i2c import CharLCD
 
 if __name__ == '__main__':
-    temperature_outside = 10
-    temperature_inside = 20
+    temperature_outside = 10.5
+    temperature_inside = 20.4
     degree_symbol = chr(223)
     lcd = CharLCD('PCF8574', 0x27)
     lcd.write_string('Out: {0}{2}C\n\rIn: {1}{2}C'.format(temperature_outside,
