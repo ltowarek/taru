8ddf09dd2e16d4142a60e7a748894dad2786404c
Lukasz Towarek <lukasz.towarek@gmail.com>
2018-10-14 14:38:06 +0200

    Add basic example of displaying temperature

---
+++ temperature_display/display.py
@@ -0,0 +1,9 @@
+from RPLCD.i2c import CharLCD
+
+if __name__ == '__main__':
+    temperature_outside = 10
+    temperature_inside = 20
+    lcd = CharLCD('PCF8574', 0x27)
+    lcd.write_string(f'Out: {temperature_outside}\u2103\n'
+                     f'In: {temperature_inside}\u2103')
+    lcd.close()
